efdafdb7c0d2d4a7de4c460ab6b1415d7cd6d4b4
ArthurSupino <161601080+ArthurSupino@users.noreply.github.com>
2024-07-06 20:01:07 -0300

    Add files via upload

---
+++ Lista08]/ContaCorrente.java
@@ -0,0 +1,38 @@
+public class ContaCorrente extends Conta {
+
+    
+    public ContaCorrente(int numero, Cliente dono, double saldo, double limite) {
+        super(numero, dono, saldo);
+        setLimite(limite);  
+    }
+    public void setLimite(double limite) {
+        if(limite > -100)
+        {
+            this.limite = limite;
+        }
+        
+    }
+    public double getLimite() {
+        return limite;
+    }
+   
+    public double calculataxas() {
+
+        double taxa_cobrada;
+        if ( this.dono.getClass() == PessoaJuridica.class) {
+                taxa_cobrada = 20;
+
+        }
+        else 
+        {
+                taxa_cobrada = 10;
+                
+        }
+        return taxa_cobrada;
+      
+       
+            
+       
+       
+    }   
+}
---
+++ Lista08]/ContaPoupanca.java
@@ -0,0 +1,24 @@
+public class ContaPoupanca extends Conta  {
+   
+    public ContaPoupanca(int numero, Cliente dono, double saldo, double limite) {
+        super(numero, dono, saldo);
+        setLimite(limite);
+    }
+
+    public void setLimite(double limit) 
+    {
+        if(limit <= 1000 && limit >=100)
+        {
+            this.limite = limit;
+        }else{this.limite = 1000;}
+        
+    }
+    public double getLimite() {
+        return limite;
+    }
+   
+    public double calculataxas() {
+      double  taxa_cobrada = 0;
+      return taxa_cobrada;
+    }
+}
